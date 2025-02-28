dfe313b9de504e0303dec2f2ec856d1bc56a20e9
felixchab <felixchabellard33@gmail.com>
2025-02-28 09:51:29 +0100

    clean us5

--- src/entities/tablePlan.entity.ts
+++ src/entities/tablePlan.entity.ts
@@ -1,11 +1,12 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 import { Table } from './table.entity';
 import { TablePlans } from '../enums/TablePlan.enum';
 
 @Entity()
 export class TablePlan {
   @Column({ unique: true })
-  id: TablePlans; // "ID" spécial pour les différents plans de tables
+  @PrimaryGeneratedColumn()
+  id: TablePlans;
 
   @Column()
   tables: Table[];
