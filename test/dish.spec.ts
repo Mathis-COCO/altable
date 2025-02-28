d083a7255340d5cf2db6d8abc0bf6b5d6df2568e
Jules Dupuis <jules.dupuis@ynov.com>
2025-02-28 10:57:30 +0100

    jest config + first test

--- test/dish.spec.ts
+++ test/dish.spec.ts
@@ -9,9 +9,44 @@ import { DishType } from './../src/enums/DishType.enum';
 
 // 1. Créer un plat
 const createDish: Dish = {
+  id: 1,
   name: 'Pizza',
   description: 'Pizza 4 fromages',
   type: DishType.PLAT_PRINCIPAL,
   price: 12.5,
   quantity: 10,
 };
+
+// ---------TESTS UNITAIRES --------- //
+
+describe('DishController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/POST create dish', () => {
+    return request(app.getHttpServer())
+      .post('/dishes')
+      .send(createDish)
+      .expect(201)
+      .expect(({ body }) => {
+        expect(body.id).toEqual(createDish.id);
+        expect(body.name).toEqual(createDish.name);
+        expect(body.description).toEqual(createDish.description);
+        expect(body.type).toEqual(createDish.type);
+        expect(body.price).toEqual(createDish.price);
+        expect(body.quantity).toEqual(createDish.quantity);
+      });
+  });
+});
